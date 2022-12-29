17ee15b9d188410ca136858c84b7722d3e7670c4
diaztoro <gilberto.diaz@gmail.com>
2022-12-29 07:00:49 -0500

    Update types.h

--- src/types.h
+++ src/types.h
@@ -5,20 +5,13 @@
 #include <zfp.h>
 #include <zfp/array2.hpp>
 #include "array2d.h"
+#include <map>
+#include <string>
 
-typedef struct {
-	double real, imag;
-} amplitude;
-
-typedef struct {
-	raw::array2d real, imag;
-} matrixcd;
 
 using AmplitudeVector = raw::array2d;
-//using Matrix = raw::array2d;
-
 using CompressedAmplitudesVector = zfp::array2d;
-using CompressedMatrix = zfp::array2d;
+typedef std::map<std::string, AmplitudeVector> state_map;
 
 
 #endif
